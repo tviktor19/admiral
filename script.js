cc153d7a8391f912fad1f1566b3f615172d7f030
Viktor Tóth <tviktor19@gmail.com>
2024-09-28 17:34:19 +0200

    about apartman gallery

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+// window.addEventListener("scroll", function () {
+//     let header = document.querySelector("header");
+//     let scrollmenu = document.querySelector(".scrollmenu");
+
+//     let sticky = header.offsetHeight; // A header magasságát használjuk referencia pontként
+
+//     if (window.pageYOffset > sticky) {
+//         header.style.display = "none"; // A header eltűnik
+//         scrollmenu.style.display = "flex"; // A scrollmenu megjelenik
+//     } else {
+//         header.style.display = "block"; // A header visszajön
+//         scrollmenu.style.display = "none"; // A scrollmenu eltűnik
+//     }
+// });
+
 window.addEventListener("scroll", function () {
     let header = document.querySelector("header");
     let scrollmenu = document.querySelector(".scrollmenu");
@@ -5,10 +20,18 @@ window.addEventListener("scroll", function () {
     let sticky = header.offsetHeight; // A header magasságát használjuk referencia pontként
 
     if (window.pageYOffset > sticky) {
-        header.style.display = "none"; // A header eltűnik
-        scrollmenu.style.display = "flex"; // A scrollmenu megjelenik
+        header.style.opacity = "0"; // A header eltűnik
+        header.style.visibility = "hidden";
+        scrollmenu.style.opacity = "1"; // A scrollmenu megjelenik
+        scrollmenu.style.visibility = "visible";
     } else {
-        header.style.display = "block"; // A header visszajön
-        scrollmenu.style.display = "none"; // A scrollmenu eltűnik
+        header.style.opacity = "1"; // A header eltűnik
+        header.style.visibility = "visible";
+        scrollmenu.style.opacity = "0"; // A scrollmenu megjelenik
+        scrollmenu.style.visibility = "hidden";
     }
+});
+
+lightbox.option({
+    albumLabel: "Kép %1 / %2",
 });
